d2669bdb5a7397bf13c3de3b8945f6899032dd09
zhuragat <zhuragat.bolatkhan@gmail.com>
2019-07-12 17:56:39 +0600

    solved 832,905,977

---
+++ array/FlippingAnImage_832/Solution.java
@@ -0,0 +1,44 @@
+package FlippingAnImage_832;
+
+
+/**
+ 832. Flipping an Image
+ https://leetcode.com/problems/flipping-an-image/
+
+ Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+
+ To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+
+ To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+
+ Example 1:
+
+ Input: [[1,1,0],[1,0,1],[0,0,0]]
+ Output: [[1,0,0],[0,1,0],[1,1,1]]
+ Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+ Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+ Example 2:
+
+ Input: [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+ Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+ Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ Notes:
+
+ 1 <= A.length = A[0].length <= 20
+ 0 <= A[i][j] <= 1
+ */
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        if (A == null || A.length == 0 || A[0].length == 0) return A;
+        int length = A.length;
+        for (int[] arr : A) {
+            for (int i = 0; i < length / 2; i++) {
+                int temp = arr[i] ^ 1;
+                arr[i] = arr[length - 1 - i] ^ 1;
+                arr[length - 1 - i] = temp;
+            }
+        }
+        return A;
+    }
+}
