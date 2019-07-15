e6de8c1c63be3013f446f282382e7727858c10ba
zhuragat <zhuragat.bolatkhan@gmail.com>
2019-07-15 16:48:48 +0600

    solved 509, 922

---
+++ array/FibonacciNumber_509/Main.java
@@ -0,0 +1,8 @@
+package array.FibonacciNumber_509;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.fib(8));
+    }
+}
