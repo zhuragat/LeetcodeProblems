68e25285bb33f586790b6fb4db44dd9fa0e5cc20
zhuragat <zhuragat.bolatkhan@gmail.com>
2019-07-16 15:15:33 +0600

    solved 999,1002

---
+++ array/AvailableCapturesForRook_999/Main.java
@@ -0,0 +1,19 @@
+package array.AvailableCapturesForRook_999;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[][] A = new char[][]{
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', 'p', '.', '.', '.', '.'},
+                {'.', '.', '.', 'B', '.', '.', '.', '.'},
+                {'.', 'p', 'B', 'R', 'p', 'B', 'p', '.'},
+                {'.', '.', '.', 'p', 'p', '.', '.', '.'},
+                {'.', '.', '.', 'B', '.', '.', '.', '.'},
+                {'.', '.', '.', 'p', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'}
+        };
+        System.out.println(solution.numRookCaptures(A));
+    }
+}
+
---
+++ array/FindCommonCharacters_1002/Main.java
@@ -0,0 +1,9 @@
+package array.FindCommonCharacters_1002;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] A = new String[]{"cool", "lock", "cook"};
+        System.out.println(solution.commonChars(A));
+    }
+}
