3245b9c791c1be408d6800e93176b81c8adcb149
zhuragat <zhuragat.bolatkhan@gmail.com>
2019-07-17 18:45:37 +0600

    solved 1089

---
+++ array/RevealCardsInIncreasingOrder_950/Main.java
@@ -0,0 +1,11 @@
+package array.RevealCardsInIncreasingOrder_950;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] A = new int[]{2, 13, 3, 11, 5, 17, 7};
+        System.out.println(Arrays.toString(solution.deckRevealedIncreasing(A)));
+    }
+}
