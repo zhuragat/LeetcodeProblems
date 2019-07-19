7e3de167766cc2624c64eaf00b63823bc62a8f11
zhuragat <zhuragat.bolatkhan@gmail.com>
2019-07-19 18:28:30 +0600

    solved 1108

---
+++ easy/string/DefangingAnIPAddress_1108/Main.java
@@ -0,0 +1,9 @@
+package easy.string.DefangingAnIPAddress_1108;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String str = "1.1.1.1";
+        System.out.println(solution.defangIPaddr(str));
+    }
+}
