4e47cc1575e1783a48eb0dfab813155ec1df6416
zhuragat <zhuragat.bolatkhan@gmail.com>
2019-07-22 22:00:59 +0600

    solved 771

---
+++ easy/hash_table/JewelsAndStones/Main.java
@@ -0,0 +1,10 @@
+package easy.hash_table.JewelsAndStones;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String J = "aA";
+        String S = "aAAbbbb";
+        System.out.println(solution.numJewelsInStones(J, S));
+    }
+}
