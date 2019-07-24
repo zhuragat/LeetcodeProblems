9c9b9feb7897fea8b281c2fe1899d41619030b45
zhuragat <zhuragat.bolatkhan@gmail.com>
2019-07-24 21:54:53 +0600

    solved 709

---
+++ easy/string/ToLowerCase_709/Main.java
@@ -0,0 +1,9 @@
+package easy.string.ToLowerCase_709;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String str = "LOVELY";
+        System.out.println(solution.toLowerCase(str));
+    }
+}
