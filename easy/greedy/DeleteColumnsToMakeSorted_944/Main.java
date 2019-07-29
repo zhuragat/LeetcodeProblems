dde84b06b213127f50f02bcfdd7568d90797907a
zhuragat <zhuragat.bolatkhan@gmail.com>
2019-07-29 17:24:13 +0600

    solved 944

---
+++ easy/greedy/DeleteColumnsToMakeSorted_944/Main.java
@@ -0,0 +1,9 @@
+package easy.greedy.DeleteColumnsToMakeSorted_944;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] A = new String[]{"zyx", "wvu", "tsr"};
+        System.out.println(solution.minDeletionSize(A));
+    }
+}
