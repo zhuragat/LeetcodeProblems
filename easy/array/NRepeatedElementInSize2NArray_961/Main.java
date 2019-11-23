afef8e6e025f424bdb7dba5643f03bb25ef5e7a2
zhuragat <zhuragat.bolatkhan@gmail.com>
2019-11-23 20:56:04 +0600

    solved 961

---
+++ easy/array/NRepeatedElementInSize2NArray_961/Main.java
@@ -0,0 +1,9 @@
+package easy.array.NRepeatedElementInSize2NArray_961;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] A = new int[]{5, 1, 5, 2, 5, 3, 5, 4};
+        System.out.println(solution.repeatedNTimes(A));
+    }
+}
