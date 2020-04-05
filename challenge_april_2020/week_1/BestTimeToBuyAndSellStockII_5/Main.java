0edd8aa722fe8378140561bbe5903f7a3c1d6467
zhuragat <zhuragat.bolatkhan@gmail.com>
2020-04-05 23:21:48 +0600

    --leetcoding-challenge-- solved problem #5 from week 1

---
+++ challenge_april_2020/week_1/BestTimeToBuyAndSellStockII_5/Main.java
@@ -0,0 +1,9 @@
+package challenge_april_2020.week_1.BestTimeToBuyAndSellStockII_5;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] A = new int[]{7,1,5,3,6,4};
+        System.out.println(solution.maxProfit(A));
+    }
+}
