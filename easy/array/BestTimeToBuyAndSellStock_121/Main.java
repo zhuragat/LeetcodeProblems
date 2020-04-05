456fa94ea52986cabb26402e8566e1b6cf020702
zhuragat <zhuragat.bolatkhan@gmail.com>
2020-04-05 16:02:57 +0600

    solved 121

---
+++ easy/array/BestTimeToBuyAndSellStock_121/Main.java
@@ -0,0 +1,9 @@
+package easy.array.BestTimeToBuyAndSellStock_121;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] A = new int[]{7,1,5,3,6,4};
+        System.out.println(solution.maxProfit(A));
+    }
+}
