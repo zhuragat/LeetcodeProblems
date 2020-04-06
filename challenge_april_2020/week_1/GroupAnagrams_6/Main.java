ea743269d290c3b3a5b5a86ce379c52886a79420
zhuragat <zhuragat.bolatkhan@gmail.com>
2020-04-06 23:40:11 +0600

    --leetcoding-challenge-- solved problem #6 from week 1

---
+++ challenge_april_2020/week_1/GroupAnagrams_6/Main.java
@@ -0,0 +1,9 @@
+package challenge_april_2020.week_1.GroupAnagrams_6;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] A = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(solution.groupAnagrams(A));
+    }
+}
---
+++ challenge_april_2020/week_1/GroupAnagrams_6/Solution.java
@@ -0,0 +1,38 @@
+package challenge_april_2020.week_1.GroupAnagrams_6;
+
+import java.util.*;
+
+/**
+ Group Anagrams
+ https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3288/
+ Given an array of strings, group anagrams together.
+
+ Example:
+
+ Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ Output:
+ [
+ ["ate","eat","tea"],
+ ["nat","tan"],
+ ["bat"]
+ ]
+ Note:
+
+ All inputs will be in lowercase.
+ The order of your output does not matter.
+ */
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = String.valueOf(arr);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
