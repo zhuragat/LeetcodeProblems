26f9764fa6df8b48a7ba4034785f1259f2098c6d
zhuragat <zhuragat.bolatkhan@gmail.com>
2020-04-07 14:43:51 +0600

    --leetcoding-challenge-- solved problem #7 from week 1

---
+++ challenge_april_2020/week_1/CountingElements_7/Main.java
@@ -0,0 +1,9 @@
+package challenge_april_2020.week_1.CountingElements_7;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] A = new int[]{1,3,2,3,5,0};
+        System.out.println(solution.countElements(A));
+    }
+}
