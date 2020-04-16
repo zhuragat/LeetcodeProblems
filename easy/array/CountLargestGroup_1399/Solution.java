311d933d9d229118ec17641f0e47c9f9f92f9384
zhuragat <zhuragat.bolatkhan@gmail.com>
2020-04-16 11:46:00 +0600

    solved 1399 (another solution)

--- easy/array/CountLargestGroup_1399/Solution.java
+++ easy/array/CountLargestGroup_1399/Solution.java
@@ -1,33 +1,32 @@
 package easy.array.CountLargestGroup_1399;
 
 class Solution {
-    public int countLargestGroup(int n) {
+    static int countLargestGroup(int n) {
         int result = 0;
         int[] arr = new int[37];
 
-        for (int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             int sum = findSum(i);
             arr[sum]++;
         }
 
         int maxSize = 0;
-        for (int i = 1; i <= 36; i++){
+        for (int i = 1; i <= 36; i++) {
             maxSize = Math.max(maxSize, arr[i]);
         }
 
-        for (int i = 1; i <= 36; i++){
-            if(arr[i] == maxSize) result++;
+        for (int i = 1; i <= 36; i++) {
+            if (arr[i] == maxSize) result++;
         }
 
         return result;
     }
 
-    int findSum(int n) {
-        int sum = 0;
-        while (n != 0) {
-            sum += n % 10;
-            n /= 10;
-        }
-        return sum;
+    static int findSum(int n) {
+        return n == 0 ? 0 : (n % 10) + findSum(n / 10);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countLargestGroup(174));
     }
 }
