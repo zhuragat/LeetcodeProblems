50a6eecfb566a3561408bec047958f832f89bc2f
zhuragat <zhuragat.bolatkhan@gmail.com>
2020-04-19 22:17:47 +0600

    --leetcoding-challenge-- solved problem #5 from week 3

---
+++ challenge_april_2020/week_3/SearchInRotatedSortedArray_5/Solution.java
@@ -0,0 +1,26 @@
+package challenge_april_2020.week_3.SearchInRotatedSortedArray_5;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            int num;
+
+            if ((nums[mid] < nums[0]) == (target < nums[0])) {
+                num = nums[mid];
+            } else {
+                num = target < nums[0] ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            if (num < target)
+                lo = mid + 1;
+            else if (num > target)
+                hi = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+}
